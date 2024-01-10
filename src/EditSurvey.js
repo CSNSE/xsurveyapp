42a2dc73732670887cdb54f3b7aeeb1426db8c63
xbb73 <xbb73@outlook.com>
2024-01-10 12:07:21 -0500

    changes i made

---
+++ src/EditSurvey.js
@@ -0,0 +1,46 @@
+import React, { Component, useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import { Nav, SurveyUpdateForm } from "./ui-components";
+import { API, Storage } from "aws-amplify";
+import "./App.css";
+import { getSurvey } from "./graphql/queries";
+function EditSurvey() {
+  return <Put />;
+}
+
+function Put() {
+  const { cid } = useParams();
+  const [cr, setMe] = useState({});
+useEffect(() => {
+    const queryData = async () => {
+      const record = cid
+        ? (
+            await API.graphql({
+              query: getSurvey.replaceAll("__typename", ""),
+              variables: { id: cid },
+            })
+          )?.data?.getSurvey
+        : cr;
+        if (record.image) {
+          record.filename = record.image
+          const url = await Storage.get(record.image);
+          record.image = url;
+          }
+        setMe(record);
+    };
+    queryData();
+  }, [cid, cr]);
+
+
+
+  return (
+    <div>
+      <header className="App-header">
+        <Nav />
+        <SurveyUpdateForm nx={cr} />
+      </header>
+    </div>
+  );
+}
+
+export default EditSurvey;
